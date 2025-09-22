28ab763ff7398fa27b144d5aad3247f35c2040e0
Daniel Casalli <danis.settk@gmail.com>
2025-09-22 09:03:40 -0300

    feat: comandos inicial do update

---
+++ queries/03-update.sql
@@ -0,0 +1,9 @@
+UPDATE usuarios SET username = 'ProGamer2025'
+WHERE username = 'player01';
+
+SELECT * FROM usuarios;
+
+UPDATE usuarios SET pontos = pontos + 1000;
+
+UPDATE usuarios SET email = 'queenbee@gmail.com'
+WHERE email = 'queenbee@email.com'
